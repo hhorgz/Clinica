12f34865cac2dbab215ddc1c7402a695126ccdba
Hanns Guzmán <6201496+hhorgz@users.noreply.github.com>
2025-02-12 12:38:32 -0600

    Merge con feature/api/doctor (#39)

---
+++ backend/techmed_backend/CHANGELOG.md
@@ -0,0 +1,4 @@
+# V0.0.1
+* API: Authentication has been disabled temporarily 
+* API: Appointment model has been corrected to work properly with its foreign keys
+
--- backend/techmed_backend/clinic/models.py
+++ backend/techmed_backend/clinic/models.py
@@ -13,16 +13,24 @@ class Patient(models.Model):
     sex = models.CharField(max_length=1, default='M', choices=SEX_CHOICES)
     # TODO: Add CUI and make a unique constraint on this field
 
+class Doctor(models.Model):
+    name = models.CharField(max_length=500)
+    cui = models.CharField(max_length=25)
+    professional_id = models.CharField(max_length=25)
+
 class AppointmentType(models.Model):
     type = models.CharField(max_length=100)
     estimated_time = models.IntegerField('duration in minutes', default=0)
+    cost = models.DecimalField(max_digits=8 ,decimal_places=2)
 
 class AppointmentStatus(models.Model):
     status = models.CharField(max_length=50)
 
 class Appointment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     appointment_type = models.ForeignKey(AppointmentType, on_delete=models.CASCADE)
     status = models.ForeignKey(AppointmentStatus, on_delete=models.CASCADE)
     scheduled_date = models.DateTimeField('appointment date YYYY-MM-DD HH:mm:ss')
     comment = models.CharField(max_length=500, null=True)
+
